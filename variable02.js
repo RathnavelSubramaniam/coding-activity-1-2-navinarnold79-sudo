029f1b7028238622947319ae08f8da549a6329ba
dhe Arnold <navinarnold79@gmail.com>
2026-08-17 13:04:04 +0530

    Add files via upload

---
+++ variable02.js
@@ -0,0 +1,8 @@
+// Array containing numbers 1 through 10
+let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Declare a variable using let
+let elementAtIndexFive = numArray[5];
+
+// Print the value to the console
+console.log("Element at index 5:", elementAtIndexFive);
